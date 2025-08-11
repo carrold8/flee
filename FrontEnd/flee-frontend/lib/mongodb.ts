3917da60f51ce3120e16c4b07165f8c101c0bd4a
carrold8 <75954494+carrold8@users.noreply.github.com>
2025-08-11 23:03:21 +0100

    bad practice

--- FrontEnd/flee-frontend/lib/mongodb.ts
+++ FrontEnd/flee-frontend/lib/mongodb.ts
@@ -1,6 +1,7 @@
 import { MongoClient } from 'mongodb';
 
-const uri = process.env.MONGODB_URI || '';
+// const uri = process.env.MONGODB_URI || '';
+const uri = 'mongodb+srv://carrold8:WuuqosEmPl7c6E6m@fleecluster.3sb6hhw.mongodb.net/?retryWrites=true&w=majority&appName=FleeCluster';
 if (!uri) throw new Error('Missing MONGODB_URI');
 
 const globalWithMongo = globalThis as typeof globalThis & {
