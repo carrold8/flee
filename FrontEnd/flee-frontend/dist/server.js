d79f468fed5e01c3bd1074835d5ae756ae18322f
carrold8 <75954494+carrold8@users.noreply.github.com>
2025-08-11 21:41:04 +0100

    Build made

--- FrontEnd/flee-frontend/dist/server.js
+++ FrontEnd/flee-frontend/dist/server.js
@@ -332,7 +332,7 @@ app.prepare().then(() => {
             }
         });
     });
-    httpServer.listen(port, () => {
+    httpServer.listen(port, '0.0.0.0', () => {
         console.log(`Server Running on http://${hostname}:${port}`);
     });
 });
